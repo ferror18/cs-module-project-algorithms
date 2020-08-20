75d6e8e0691f816be5ff62e117d70af55f03d329
ferror18 <ferror18@gmail.com>
2020-08-19 18:42:35 -0700

    Product of all other numbers FP

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
-
+def verbose(myFunction, prt_able_i, do=False):
+    if do:
+        print(f'{myFunction.__name__} --> {prt_able_i}')
 import sys
 
 def rock_paper_scissors(n):
   # Your code here
 
-  pass
+  for i in n:
+    print(i)
 
 
 if __name__ == "__main__":
